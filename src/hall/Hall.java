c1ceaa30a6f206ab003de293b1e9678dcd717a76
Xiaohan <xiaohan.zhu@outlook.com>
2018-05-01 16:27:20 +1000

    key input
    temp

--- src/hall/Hall.java
+++ src/hall/Hall.java
@@ -3,19 +3,17 @@ package hall;
 import main.ReadyPlayerOne;
 import utils.Const;
 import utils.GameSystem;
-import utils.KeyInput;
 
 public class Hall extends GameSystem {
+  HallPlayer player = new HallPlayer(20, 20);
   public Hall(ReadyPlayerOne g) {
     super(g);
   }
   
   @Override
   public void run() {
-    g.background(Const.BLACK);
-    g.ellipse(Const.WIDTH / 2, Const.HEIGHT / 2, 100, 100);
-    if (KeyInput.isLeftPressed) {
-      g.ellipse(100, 100, 100, 100);
-    }
+    g.background(Const.WHITE);
+    player.update();
+    player.display(g);
   }
 }
---
+++ src/hall/HallPlayer.java
@@ -0,0 +1,32 @@
+package hall;
+
+import processing.core.PApplet;
+import utils.Const;
+import utils.KeyInput;
+import utils.MovingObject;
+
+public class HallPlayer extends MovingObject {
+  public HallPlayer(float _x, float _y) {
+    super(_x, _y);
+  }
+  
+  @Override
+  public void update() {
+    System.out.println(x);
+    if (KeyInput.isLeftPressed) {
+      x -= VELOCITY;
+    } else if (KeyInput.isRightPressed) {
+      x += VELOCITY;
+    }
+    if (x >= Const.WIDTH) {
+      x -= VELOCITY;
+    } else if (x <= 0) {
+      x += VELOCITY;
+    }
+  }
+
+  @Override
+  public void display(PApplet g) {
+    g.ellipse(x, y, 20, 20);
+  }
+}
